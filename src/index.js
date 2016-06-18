595e1fd9e77c0f88d40deb2f532e3f508f697770
Eric Anderson <eric@ericanderson.ca>
2016-06-18 17:21:42 -0600

    Use JSON instead of YAML.

--- src/index.js
+++ src/index.js
@@ -7,24 +7,7 @@ const pkg = require('../package.json');
 const gulp = require('gulp');
 
 // Utilities
-const fs   = require('fs');
-const yaml = require('js-yaml');
 const gutil = require('gulp-util');
-
-// JS building
-const babelify = require('babelify');
-const browserify = require('browserify');
-const uglify = require('gulp-uglify');
-
-// Style building
-const minifyCSS = require('gulp-minify-css');
-const postcss = require('gulp-postcss');
-const postcssCSSNext = require('postcss-cssnext');
-const postcssImport = require('postcss-import');
-const postcssReporter = require('postcss-reporter');
-
-// Server running
-const connect = require('gulp-connect');
 
 let taskMap = {
   "javascript modules": require('./tasks/javascript/modules.js'),
@@ -32,24 +15,16 @@ let taskMap = {
   "css imports":        require('./tasks/css/imports.js')
 };
 let taskNames = [];
+let manifest = require('../manifest.json');
 
-// Get document, or throw exception on error
-fs.readFile('./manifest.yml', 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    let manifest = yaml.safeLoad(data);
+for (let i=0; i < manifest.tasks.length; i++) {
+  let details = manifest.tasks[i];
+  taskNames.push(details.name);
 
-    for (let i=0; i < manifest.tasks.length; i++) {
-      let details = manifest.tasks[i];
-      taskNames.push(details.name);
+  // Define the Gulp taks
+  gulp.task(details.name, taskMap[details.what](details));
+}
 
-      // Define the Gulp taks
-      gulp.task(details.name, taskMap[details.what](details));
-    }
-
-    // Run gulp!
-    gulp.task('default', taskNames);
-    gulp.start();
-  }
-});
+// Run gulp!
+gulp.task('default', taskNames);
+gulp.start();
