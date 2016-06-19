4c6022b02e5a3810de8b8b96ae2b9afa6c3d4fba
Eric Anderson <eric@ericanderson.ca>
2016-06-19 15:45:58 -0600

    Minification and concatenation tasks.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 'use strict'
 
-// The package.json
 const pkg = require('../package.json');
-
-// Gulp, of course!
 const gulp = require('gulp');
-
-// Utilities
 const gutil = require('gulp-util');
 
 let taskMap = {
   // JS Tasks
   "javascript modules":           require('./tasks/javascript/modules.js'),
-  "minify javascript file":        require('./tasks/javascript/minify.js'),
+  "minify javascript file":       require('./tasks/javascript/minify.js'),
   
   // CSS Tasks
   "css imports":                  require('./tasks/css/imports.js'),
@@ -20,6 +15,9 @@ let taskMap = {
 
   // Server Tasks
   "server":                       require('./tasks/javascript/server.js'),
+
+  // General Tasks
+  "concatenate":                  require('./tasks/common/concatenate.js')
 };
 let taskNames = [];
 let manifest = require('../manifest.json');
@@ -31,20 +29,30 @@ for (let i=0; i < manifest.tasks.length; i++) {
   // Define the Gulp taks
   gulp.task(details.name, taskMap[details.what](details));
 
-  if (details.minify_after) {
-    details.input = details.output;
-    let taskName = `minify-${details.name}`;
-    taskNames.push(taskName);
-
-    if (details.what.indexOf('javascript') > -1) {
-      gulp.task(taskName, [details.name], taskMap['minify javascript file'](details));
-    }
-    else if (details.what.indexOf('css') > -1) {
-      gulp.task(taskName, [details.name], taskMap['minify css file'](details));
-    }
-  }
+  // Add minification task if required
+  if (details.minify_after) createMinificationTask(details);
 }
 
 // Run gulp!
 gulp.task('default', taskNames);
 gulp.start();
+
+// ***************************
+// Utility functions
+// ***************************
+
+function createMinificationTask (details) {
+  details.input = details.output;
+  let taskName = `minify-${details.name}`;
+  taskNames.push(taskName);
+
+  let splitPath = details.output.split('.');
+  let fileType = splitPath[splitPath.length-1];
+
+  if (fileType === 'js') {
+    gulp.task(taskName, [details.name], taskMap['minify javascript file'](details));
+  }
+  if (fileType === 'css') {
+    gulp.task(taskName, [details.name], taskMap['minify css file'](details));
+  }
+}
--- src/tasks/common/concatenate.js
+++ src/tasks/common/concatenate.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const gulp = require('gulp');
-const minifyCSS = require('gulp-minify-css');
-const rename = require('gulp-rename');
+const gutil = require('gulp-util');
+const concat = require('gulp-concat');
 const Utils = require('../../utils/index.js');
 
 module.exports = function (details) {
   let outputs = Utils.parseOutput(details.output);
 
   return function () {
-    return gulp.src(details.input)
-      .pipe(minifyCSS({ compatibility: 'ie9' }))
-      .pipe(rename({ suffix: '.min' }))
+    return gulp.src(details.files)
+      .pipe(concat(outputs.filename))
       .pipe(gulp.dest(outputs.dir));
   }
 };
