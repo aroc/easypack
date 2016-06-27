5e37186b13b4ea522ddca9c43b443955d8362e9a
aroc <eric@ericanderson.ca>
2016-06-27 12:39:24 -0600

    Update server task. No more rootPath.

--- src/tasks/utils/server.js
+++ src/tasks/utils/server.js
@@ -5,12 +5,12 @@ const gutil = require('gulp-util');
 const connect = require('gulp-connect');
 var Utils = require('../../utils.js');
 
-module.exports = function (details) {
+module.exports = function (details) { 
   gutil.log(`Starting ${details.name}...`);
   
   return function () {
     return connect.server({
-      root: details.paths.map(p => path.resolve(details.rootPath + '/' + p)),
+      root: details.paths.map(p => path.resolve(p)),
       port: details.port || 8080
     });
   }
